5f9e08294ad447b90d369080e7ec28328b9cd428
Nick Zelei <nick.zelei@garmin.com>
2016-08-12 13:41:58 -0500

    added void return type

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
-export function is_uri(value: string): string;
-export function isUri(value: string): string;
+export function is_uri(value: string): string | void;
+export function isUri(value: string): string | void;
 
-export function is_http_uri(value: string, allowHttps?: boolean): string;
-export function isHttpUri(value: string, allowHttps?: boolean): string;
+export function is_http_uri(value: string, allowHttps?: boolean): string | void;
+export function isHttpUri(value: string, allowHttps?: boolean): string | void;
 
-export function is_https_uri(value: string): string;
-export function isHttpsUri(value: string): string;
+export function is_https_uri(value: string): string | void;
+export function isHttpsUri(value: string): string | void;
 
-export function is_web_uri(value: string): string;
-export function isWebUri(value: string): string;
+export function is_web_uri(value: string): string | void;
+export function isWebUri(value: string): string | void;
