c361ad805b5b79c308be9bb52d855d9ca5f0c958
Nick Zelei <nick.zelei@garmin.com>
2016-08-12 13:25:43 -0500

    removed namespace

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,11 @@
-declare namespace validUrl {
-    export function is_uri(value: string): string;
-    export function isUri(value: string): string;
+export function is_uri(value: string): string;
+export function isUri(value: string): string;
 
-    export function is_http_uri(value: string, allowHttps?: boolean): string;
-    export function isHttpUri(value: string, allowHttps?: boolean): string;
+export function is_http_uri(value: string, allowHttps?: boolean): string;
+export function isHttpUri(value: string, allowHttps?: boolean): string;
 
-    export function is_https_uri(value: string): string;
-    export function isHttpsUri(value: string): string;
+export function is_https_uri(value: string): string;
+export function isHttpsUri(value: string): string;
 
-    export function is_web_uri(value: string): string;
-    export function isWebUri(value: string): string;
-}
-
-export = validUrl;
+export function is_web_uri(value: string): string;
+export function isWebUri(value: string): string;
--- test/test.ts
+++ test/test.ts
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 import test = require('blue-tape');
 
 import validUrl = require('valid-url');
 
 
-test("default example", (t) => {
-    const invalidUrl: string = "not-valid";
-    const realUrl: string = "https://www.google.com/";
+test('default example', (t) => {
+    const invalidUrl: string = 'not-valid';
+    const realUrl: string = 'https://www.google.com/';
     t.equal(validUrl.isUri(realUrl), realUrl);
     t.equal(validUrl.isUri(invalidUrl), undefined);
 
