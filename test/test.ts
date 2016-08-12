7a7191b549c3980951281794cba2496ab0b9af07
Nick Zelei <nick.zelei@garmin.com>
2016-08-12 14:37:51 -0500

    added new test to check type on valid return

--- test/test.ts
+++ test/test.ts
@@ -3,12 +3,21 @@ import test = require('blue-tape');
 
 import validUrl = require('valid-url');
 
+const invalidUrl: string = 'not-valid';
+const realUrl: string = 'https://www.google.com/';
 
 test('default example', (t) => {
-    const invalidUrl: string = 'not-valid';
-    const realUrl: string = 'https://www.google.com/';
     t.equal(validUrl.isUri(realUrl), realUrl);
     t.equal(validUrl.isUri(invalidUrl), undefined);
+
+    t.end();
+});
+
+test('returns string', (t) => {
+    let returnedRealUrl = validUrl.isUri(realUrl);
+    if (typeof returnedRealUrl === 'string') {
+        returnedRealUrl.substring(4);
+    }
 
     t.end();
 });
